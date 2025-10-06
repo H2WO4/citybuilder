4e207f02e11335a8733fc8c59ebf79ce76db4a65
H2WO4 <arthurlamidel@gmail.com>
2025-10-06 14:30:30 +0200

    Finish DB model

--- server/migrations/20251006094744-Initial.js
+++ server/migrations/20251006094744-Initial.js
@@ -1,27 +1,81 @@
-module.exports = {
-  /**
-   * @param db {import('mongodb').Db}
-   * @param client {import('mongodb').MongoClient}
-   * @returns {Promise<void>}
-   */
-  async up(db, client) {
-    await db.createCollection("Cities", {
-      validator: {
-        $jsonSchema: {
-          
-        }
-      }
-    })
-  },
+export async function up(db, client) {
+  await db.createCollection("Cities", {
+    validator: {
+      $jsonSchema: {
+        required: ["_id", "name"],
 
-  /**
-   * @param db {import('mongodb').Db}
-   * @param client {import('mongodb').MongoClient}
-   * @returns {Promise<void>}
-   */
-  async down(db, client) {
-    // TODO write the statements to rollback your migration (if possible)
-    // Example:
-    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
-  }
-};
+        properties: {
+          _id: { bsonType: "objectId" },
+
+          name: { bsonType: "string" }
+        },
+        additionalProperties: false
+      },
+      $expr: {
+        $ne: [
+          "$name",
+          ""
+        ]
+      }
+    }
+  })
+
+  await db.createCollection("Buildings", {
+    validator: {
+      $jsonSchema: {
+        required: ["_id", "city", "position", "type"],
+
+        properties: {
+          _id: { bsonType: "objectId" },
+
+          city: { bsonType: "objectId" },
+
+          position: {
+            bsonType: "object",
+            properties: {
+              x: { bsonType: "int" },
+              y: { bsonType: "int" },
+            }
+          },
+
+          type: {
+            enum: ["residential", "commercial", "industry", "entertainment", "services"]
+          }
+        },
+        additionalProperties: false
+      }
+    }
+  })
+
+  await db.createCollection("Contracts", {
+    validator: {
+      $jsonSchema: {
+        required: ["_id", "cityA", "cityB", "money", "food", "electricity", "water"],
+
+        properties: {
+          _id: { bsonType: "objectId" },
+
+          cityA: { bsonType: "objectId" },
+          cityB: { bsonType: "objectId" },
+
+          ressources: {
+            bsonType: "object",
+            properties: {
+              money: { bsonType: "int" },
+              food: { bsonType: "int" },
+              electricity: { bsonType: "int" },
+              water: { bsonType: "int" },
+            }
+          }
+        },
+        additionalProperties: false
+      }
+    }
+  })
+}
+
+export async function down(db, client) {
+  await db.dropCollection("Cities");
+  await db.dropCollection("Buildings");
+  await db.dropCollection("Contracts");
+}
